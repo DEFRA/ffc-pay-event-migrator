18ea43023728ca788bdbc64f341322fa88f5e01b
John Watson <john.watson1@defra.gov.uk>
2023-04-26 09:40:50 +0100

    Add config

--- app/config.js
+++ app/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   storageConnectionString: process.env.STORAGE_CONNECTION_STRING,
-  storageTableName: process.env.STORAGE_TABLE_NAME || 'payeventstore'
+  v1Table: 'payeventstore',
+  paymentTable: 'payments',
+  batchTable: 'batches',
+  holdTable: 'holds',
+  warningTable: 'warnings'
 }
