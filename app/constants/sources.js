8499fd81d08f8157b7d21d94fde55eed8af01bcd
John Watson <john.watson1@defra.gov.uk>
2023-04-27 15:50:51 +0100

    Add sources

---
+++ app/constants/sources.js
@@ -0,0 +1,8 @@
+module.exports = {
+  BATCH_PROCESSOR: 'ffc-pay-batch-processor',
+  ENRICHMENT: 'ffc-pay-enrichment',
+  PROCESSING: 'ffc-pay-processing',
+  SUBMISSION: 'ffc-pay-submission',
+  RESPONSES: 'ffc-pay-responses',
+  REQUEST_EDITOR: 'ffc-pay-request-editor'
+}
