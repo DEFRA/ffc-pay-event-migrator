b31ba53d9ee97b011c5c2f6c361f61b267435ecb
John Watson <john.watson1@defra.gov.uk>
2023-04-27 15:48:22 +0100

    Add event map

--- app/constants/v2-events.js
+++ app/constants/v2-events.js
@@ -12,5 +12,19 @@ module.exports = {
   PAYMENT_PROCESSED: 'uk.gov.defra.ffc.pay.payment.processed',
   PAYMENT_SUBMITTED: 'uk.gov.defra.ffc.pay.payment.submitted',
   PAYMENT_ACKNOWLEDGED: 'uk.gov.defra.ffc.pay.payment.acknowledged',
-  PAYMENT_SETTLED: 'uk.gov.defra.ffc.pay.payment.settled'
+  PAYMENT_SETTLED: 'uk.gov.defra.ffc.pay.payment.settled',
+  BATCH_REJECTED: 'uk.gov.defra.ffc.pay.warning.batch.rejected',
+  BATCH_PROCESSED: 'uk.gov.defra.ffc.pay.batch.processed.siti-agri',
+  BATCH_QUARANTINED: 'uk.gov.defra.ffc.pay.warning.batch.quarantined',
+  PAYMENT_REJECTED: 'uk.gov.defra.ffc.pay.warning.payment.rejected',
+  PAYMENT_DAX_REJECTED: 'uk.gov.defra.ffc.pay.warning.payment.dax.rejected',
+  PAYMENT_INVALID_BANK: 'uk.gov.defra.ffc.pay.warning.bank.missing',
+  PAYMENT_PAUSED_PREFIX: 'uk.gov.defra.ffc.pay.payment.paused',
+  PAYMENT_PROCESSING_FAILED: 'uk.gov.defra.ffc.pay.warning.processing.failed',
+  PAYMENT_SETTLEMENT_UNMATCHED: 'uk.gov.defra.ffc.pay.warning.settlement.unmatched',
+  BATCH_CREATED: 'uk.gov.defra.ffc.pay.batch.created.dax',
+  RESPONSE_REJECTED: 'uk.gov.defra.ffc.pay.warning.response.rejected',
+  PAYMENT_REQUEST_BLOCKED: 'uk.gov.defra.ffc.pay.warning.payment.debt.missing',
+  PAYMENT_REQUEST_ENRICHED: 'uk.gov.defra.ffc.pay.payment.debt.attached',
+  LEDGER_ASSIGNMENT_REVIEWED: 'uk.gov.defra.ffc.pay.payment.ledger.assigned'
 }
