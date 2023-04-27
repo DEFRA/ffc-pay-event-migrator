6aeca4af2da7818b7e209ee03e3a42dcee21dc8a
John Watson <john.watson1@defra.gov.uk>
2023-04-27 16:03:47 +0100

    Add type and source mapping

--- app/migration/run-migration.js
+++ app/migration/run-migration.js
@@ -4,15 +4,25 @@ const v1Client = TableClient.fromConnectionString(storageConnectionString, v1Tab
 const paymentClient = TableClient.fromConnectionString(storageConnectionString, paymentTable, { allowInsecureConnection: true })
 const batchClient = TableClient.fromConnectionString(storageConnectionString, batchTable, { allowInsecureConnection: true })
 const warningClient = TableClient.fromConnectionString(storageConnectionString, warningTable, { allowInsecureConnection: true })
+const eventMap = require('../constants/event-map')
 
 const runMigration = async () => {
   const events = []
   const eventResults = v1Client.listEntities()
   for await (const event of eventResults) {
-    events.push(event.EventType)
+    const newEvent = createNewEvent(event)
+    events.push(newEvent)
   }
 
   console.log([...new Set(events)])
+}
+
+const createNewEvent = (event) => {
+  const mappedEvent = eventMap[event.EventType]
+  return {
+    type: mappedEvent.v2,
+    source: mappedEvent.source
+  }
 }
 
 module.exports = {
