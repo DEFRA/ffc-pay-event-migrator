fdb86978b0b33d3b12be469c2e10655bf01d98cf
John Watson <johnwatson484@hotmail.com>
2023-04-29 15:51:59 +0100

    Handle duplicate warnings

--- app/migration/save-event/create-if-not-exists.js
+++ app/migration/save-event/create-if-not-exists.js
@@ -1,8 +1,14 @@
 const { odata } = require('@azure/data-tables')
 const { completeMigration } = require('../../config')
+const { WARNING } = require('../../constants/categories')
 
 const createIfNotExists = async (client, entity) => {
-  const existingEvents = client.listEntities({ queryOptions: { filter: odata`PartitionKey eq ${entity.partitionKey.toString()} and RowKey eq ${entity.rowKey.toString()}` } })
+  let existingEvents
+  if (entity.category === WARNING) {
+    existingEvents = client.listEntities({ queryOptions: { filter: odata`PartitionKey eq ${entity.partitionKey.toString()} and time eq ${entity.time.toString()}` } })
+  } else {
+    existingEvents = client.listEntities({ queryOptions: { filter: odata`PartitionKey eq ${entity.partitionKey.toString()} and RowKey eq ${entity.rowKey.toString()}` } })
+  }
   let matches = 0
   for await (const _ of existingEvents) { // eslint-disable-line no-unused-vars
     matches++
