884c9b686cde821b64c741778a74a112eaf98df5
John Watson <johnwatson484@hotmail.com>
2023-04-29 17:10:53 +0100

    Add ack event

--- app/migration/get-payment-request.js
+++ app/migration/get-payment-request.js
@@ -7,10 +7,18 @@ const getPaymentRequest = async (eventType, v1Event) => {
   const v1Client = TableClient.fromConnectionString(storageConnectionString, v1Table, { allowInsecureConnection: true })
   const submissionEvents = []
   let events
-  if (eventType === PAYMENT_DAX_REJECTED || eventType === PAYMENT_ACKNOWLEDGED) {
+  if (eventType === PAYMENT_DAX_REJECTED) {
     events = await v1Client.listEntities({ queryOptions: { filter: odata`EventType eq 'payment-request-submission-batch'` } })
     for await (const event of events) {
       if (event.Payload.includes(v1Event.properties.action.data.acknowledgement.invoiceNumber)) {
+        const sanitizedV1Event = sanitizeV1Event(event)
+        submissionEvents.push(sanitizedV1Event)
+      }
+    }
+  } else if (eventType === PAYMENT_ACKNOWLEDGED) {
+    events = await v1Client.listEntities({ queryOptions: { filter: odata`EventType eq 'payment-request-submission-batch'` } })
+    for await (const event of events) {
+      if (event.Payload.includes(v1Event.properties.action.data.invoiceNumber)) {
         const sanitizedV1Event = sanitizeV1Event(event)
         submissionEvents.push(sanitizedV1Event)
       }
